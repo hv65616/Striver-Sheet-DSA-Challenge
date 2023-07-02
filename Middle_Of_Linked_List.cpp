29ddf67694ed6f8dc890930fcb33a4ef93c200de
Himanshu <hv65616@gmail.com>
2023-07-03 00:37:42 +0530

    solution of middle of linked list

---
+++ Middle_Of_Linked_List.cpp
@@ -0,0 +1,46 @@
+/*
+Following is the class structure of the Node class:
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node()
+    {
+        this->data = 0;
+        next = NULL;
+    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+    Node(int data, Node* next)
+    {
+        this->data = data;
+        this->next = next;
+    }
+};
+*/
+int length(Node *head)
+{
+    Node *temp = head;
+    int len = 0;
+    while (temp != NULL)
+    {
+        len++;
+        temp = temp->next;
+    }
+    return len;
+}
+Node *findMiddle(Node *head)
+{
+    int len = length(head) / 2;
+    while (len)
+    {
+        head = head->next;
+        len--;
+    }
+    return head;
+}
